4ab2a01295e9d082a527d2c4347f2cec639add1b
asifdnb <143398097+asifdnb@users.noreply.github.com>
2023-08-28 13:40:21 +0200

    Add files via upload
    sql1

---
+++ sql1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM "ECONOMY"."BEANIPA" WHERE "Table Name" = 'Percent Change From Preceding Period In Real Gross Domestic Product' AND "Indicator Name" = 'Gross domestic product (GDP)' AND "Frequency" = 'Q' ORDER BY "Date";
+
+ 
