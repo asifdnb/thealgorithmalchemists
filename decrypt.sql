eafd735781701e87e4f494af45968c7294fb6ce6
khan0047@gmail.com <khan0047@gmail.com>
2023-08-29 08:35:33 +0000

    a

---
+++ decrypt.sql
@@ -0,0 +1,3 @@
+SELECT DECRYPT(C_NAME_encypted, 'encryption_key') AS original_email
+FROM OURDB.PUBLIC.V1;
+
---
+++ encrypt.sql
@@ -0,0 +1,8 @@
+
+UPDATE OURDB.PUBLIC.V1
+SET C_NAME_encypted = ENCRYPT(C_NAME, 'encryption_key');
+
+UPDATE
+set
+From 
+where
